18ec4ccd09645ef048942e3e3508658d3525d7f5
timtom <87718978+programmerlessons@users.noreply.github.com>
2023-02-21 18:46:47 +0530

    Add files via upload

---
+++ my_js.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+//Id Change
+const my_title=document.getElementById("title_p");
+my_title.style.fontFamily="Times New Roman";
+
+
+//Class Change
+const image_change=document.getElementsByClassName("image");
+image_change[0].style.height="350px";
+image_change[1].style.height="350px";
+image_change[2].style.height="350px";
+image_change[3].style.height="350px";
+
+//Tag change
+const tag_change=document.getElementsByTagName("p");
+tag_change[0].style.fontFamily="Franklin Gothic Medium";
+tag_change[1].style.fontFamily="Franklin Gothic Medium";
+tag_change[2].style.fontFamily="Franklin Gothic Medium";
+tag_change[3].style.fontFamily="Franklin Gothic Medium";
+
+const name_change=document.getElementsByName("logos");
+name_change.height="200px";
+
+
+
+
+
+
